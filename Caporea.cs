139d2071c44ef605b67c6a8f76879a1e69822412
teyezz <122233357+teyezz@users.noreply.github.com>
2023-12-08 15:03:40 +0900

    Add files via upload

---
+++ Caporea.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caporea : MonoBehaviour, IActiveSkill
+{   //스킬의 동작부를 따로 분리한다. 정보는 SKD에서 관리하며 이걸로 정보를 주고 받는다
+    //so 스킬 1개에 2개 스크립트를 유지한다
+    public void ActiveAction(SKD skilldata)
+    {
+
+    }
+}
