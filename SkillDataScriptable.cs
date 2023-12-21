b14cd29842c94945fbb88e25bba5b46a44f96601
teyezz <122233357+teyezz@users.noreply.github.com>
2023-12-21 17:36:16 +0900

    Update SkillDataScriptable.cs

--- SkillDataScriptable.cs
+++ SkillDataScriptable.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Skilldata" , menuName = "Skill/skill", order = int.MaxValue)]
 public class SkillDataScriptable : ScriptableObject
 {
+    public bool isEmptyCheck;
+    public bool isProjectile;
+    public GameObject Projectlie;
     public AnimationClip animationClip;
     public Sprite sprite;
     public float colltime;
